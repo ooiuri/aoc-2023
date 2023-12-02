3441a910116085ce7f2f1a949c188a308007303d
Iuri Everton <ieverton@aimirimsti.com.br>
2023-12-02 13:27:23 -0300

    finishing part2

--- 01/main.cpp
+++ 01/main.cpp
@@ -4,24 +4,33 @@
 #include <fstream>
 #include <string>
 
-bool isDigit(char character) {
-    if (character >= '0' && character <= '9') {
+std::string numbers[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+bool isDigit(char character)
+{
+    if (character >= '0' && character <= '9')
+    {
         return true;
     }
     return false;
 }
 
-int parseNumber(std::string line) {
+int parseNumber(std::string line)
+{
     char first, last;
     const char *str = line.c_str();
-    for (int i = 0; i < line.length(); i++) {
-        if(isDigit(str[i])){
+    for (int i = 0; i < line.length(); i++)
+    {
+        if (isDigit(str[i]))
+        {
             first = str[i];
             break;
         }
     }
-    for (int i = line.length(); i >= 0; i--) {
-        if(isDigit(str[i])){
+    for (int i = line.length(); i >= 0; i--)
+    {
+        if (isDigit(str[i]))
+        {
             last = str[i];
             break;
         }
@@ -31,20 +40,81 @@ int parseNumber(std::string line) {
     return std::atoi(result);
 }
 
-int main() {
+int findNumber (std::string line) {
+    int firstPos = line.length();
+    int lastPos = -1;
+    char first, last;
+    const char* linech = line.c_str();
+    
+    for(int i = 0; i < 10; i++){
+        size_t pos = line.find(numbers[i]);
+        if (pos != std::string::npos) {
+            if( pos < firstPos) {
+                firstPos = pos;
+                first = '0' + i; 
+            }
+        }
+    }
+    for(int i = 0; i < 10; i++){
+        size_t found = line.find(numbers[i]);
+        size_t pos = std::string::npos;
+
+        while (found != std::string::npos) {
+            pos = found;
+            found = line.find(numbers[i], found + 1);
+        }
+        if (pos != std::string::npos) {
+            if( pos > lastPos || lastPos == -1) {
+                printf("entrei\n");
+
+                lastPos = pos;
+                last = '0' + i; 
+            }
+        }
+    }
+
+    for(int i = 0; i < line.length(); i++){
+        if (isDigit(linech[i])){
+            if( i < firstPos) {
+                firstPos = i;
+                first = linech[i];
+            }
+            if( i > lastPos) {
+                lastPos = i;
+                last = linech[i];
+            }
+        }
+        
+    }
+
+    char result[3];
+    sprintf(result, "%c%c", first, last);
+    return std::atoi(result);
+}
+
+int main()
+{
 
     printf("Hello Advent! - 01\n");
-    std::ifstream inputFile("input.txt"); 
+    std::ifstream inputFile("input.txt");
     int result = 0;
-    if (inputFile.is_open()) { 
+    if (inputFile.is_open())
+    {
         std::string line;
-        while (std::getline(inputFile, line)) { 
-            result += parseNumber(line);
+        int i = 1;
+        while (std::getline(inputFile, line))
+        {   
+            int n = findNumber(line);
+            printf("%d\n", n);
+            result += n;
+            i++;
         }
-        inputFile.close(); 
-    } else {
+        inputFile.close();
+    }
+    else
+    {
         printf("Erro ao abrir! \n");
     }
 
-    printf("result: %d\n", result);
+    printf("\nresult: %d\n", result);
 }
